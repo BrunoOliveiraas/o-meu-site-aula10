c9980f937b4b42670650aa4e246a5d2ffebba2cb
BrunoOliveiraas <145805188+BrunoOliveiraas@users.noreply.github.com>
2023-12-02 21:20:17 +0000

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,31 @@
+
+
+// Crie variáveis com seu nome e idade
+var meuNome = "Bruno";
+var minhaIdade = 20;
+
+// Multiplique sua idade por 7 e guarde o resultado em uma variável
+var idadeVezesSete = minhaIdade * 7;
+
+// Crie um array com 3 nomes de amigos
+var nomesAmigos = ["Jorge", "Carlos", "Maria"];
+
+// Crie um objeto com suas características (nome, idade, é estudante?)
+var minhasCaracteristicas = {
+    nome: meuNome,
+    idade: minhaIdade,
+    estudante: true // ou false, dependendo se é estudante ou não
+};
+
+// Crie um array de objetos com as características de 3 amigos
+var amigos = [
+    { nome: "Jorge", idade: 30, estudante: false },
+    { nome: "Carlos", idade: 28, estudante: true },
+    { nome: "Maria", idade: 26, estudante: true }
+];
+
+// Exiba os resultados na consola (pode ser visualizado na consola no navegador)
+console.log("Idade vezes sete:", idadeVezesSete);
+console.log("Nomes dos amigos:", nomesAmigos);
+console.log("Minhas características:", minhasCaracteristicas);
+console.log("Características dos amigos:", amigos);
